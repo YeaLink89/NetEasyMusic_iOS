759b3118ae9e038bd48f90d01e52ed8132cd21d4
chenhao <hoachen91@gmail.com>
2016-10-15 23:08:39 +0800

    commit discover page top tab

--- NetEasyMusic_iOS/DiscoverViewController.m
+++ NetEasyMusic_iOS/DiscoverViewController.m
@@ -7,16 +7,18 @@
 //
 
 #import "DiscoverViewController.h"
-#import "iCarousel.h"
-#import "XTSegmentControl.h"
+#import "HYPageView.h"
+#import "PlayListCollectionViewCell.h"
 
-@interface DiscoverViewController () <iCarouselDataSource,iCarouselDelegate>
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
 
-@property (nonatomic , strong) iCarousel *carousel;
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 
-@property (nonatomic , strong) XTSegmentControl *segmentControl;
+@interface DiscoverViewController ()
+
 
 @property (nonatomic , strong) NSArray *titleArray;
+
 @end
 
 @implementation DiscoverViewController
@@ -30,35 +32,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSArray *defaultTitles = @[@"个性推荐",@"歌单", @"主播电台", @"排行榜"];
-    _titleArray = defaultTitles;
-    
-    float listOrigionY = kIOS7DIS(64) + kSegMentControlHeight;
-    float listViewHeight = CGRectGetHeight(self.view.bounds) - 44 - kIOS7DIS(20) - kSegMentControlHeight;
-    _carousel = [[iCarousel alloc] initWithFrame:CGRectMake(0, listOrigionY, 320, listViewHeight)];
-    _carousel.backgroundColor = [UIColor whiteColor];
-    _carousel.dataSource = self;
-    _carousel.delegate = self;
-    _carousel.decelerationRate = 0.7;
-    _carousel.type = iCarouselTypeLinear;
-    _carousel.pagingEnabled = YES;
-    _carousel.edgeRecognition = YES;
-    _carousel.bounceDistance = 0.4;
-    [self.view addSubview:_carousel];
-    
-    __weak typeof(_carousel) weakCarousel = _carousel;
-    
-    _segmentControl = [[XTSegmentControl alloc] initWithFrame:CGRectMake(20, kIOS7DIS(64), 260, 36) Items:_titleArray selectedBlock:^(NSInteger index) {
-        
-        [weakCarousel scrollToItemAtIndex:index animated:NO];
-    }];
-    
-    [self.view addSubview:_segmentControl];
-    
-//    
-//    XTCornerButton *cornerButton = [XTCornerButton buttonWithType:UIButtonTypeCustom];
-//    cornerButton.frame = CGRectMake(CGRectGetMaxX(_segmentControl.frame) + 1, CGRectGetMinY(_segmentControl.frame), CGRectGetWidth(self.view.bounds) - CGRectGetMaxX(_segmentControl.frame), CGRectGetHeight(_segmentControl.frame));
-//    [cornerButton addTarget:self action:@selector(showCategories:) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:cornerButton];
+    HYPageView *pageView = [[HYPageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT) withTitles:defaultTitles withViewControllers:nil withParameters:nil];
+    pageView.isTranslucent = NO;
+    pageView.selectedColor = [UIColor redColor];
+    pageView.unselectedColor = [UIColor blackColor];
+    [self.view addSubview:pageView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -75,40 +53,6 @@
     // Pass the selected object to the new view controller.
 }
 */
-
-
-
-- (NSUInteger)numberOfItemsInCarousel:(iCarousel *)carousel
-{
-    return _titleArray.count;
-}
-
-- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
-{
-
-    return nil;
-}
-
-- (void)carouselDidScroll:(iCarousel *)carousel
-{
-    if (_segmentControl) {
-        
-        float offset = carousel.scrollOffset;
-        
-        if (offset > 0) {
-            
-            [_segmentControl moveIndexWithProgress:offset];
-        }
-    }
-}
-
-- (void)carouselDidEndScrollingAnimation:(iCarousel *)carousel
-{
-    if (_segmentControl) {
-        
-        [_segmentControl endMoveIndex:carousel.currentItemIndex];
-    }
-}
 
 
 @end
---
+++ NetEasyMusic_iOS/PlayListCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  PlayListCollectionViewCell.m
+//  NetEasyMusic_iOS
+//
+//  Created by chenhao on 16/10/15.
+//  Copyright © 2016年 com.chh. All rights reserved.
+//
+
+#import "PlayListCollectionViewCell.h"
+
+@implementation PlayListCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
